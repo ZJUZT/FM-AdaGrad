641026e5837c41ba2e9069aa50246ebc0426ef27
Teng Zhang <zhangteng398@gmail.com>
2018-01-18 22:32:39 +0800

    Update DKNN_LLFM_SGD_multi.m

--- matlab/DKNN_LLFM_SGD_multi.m
+++ matlab/DKNN_LLFM_SGD_multi.m
@@ -9,14 +9,15 @@ class_num = max(train_Y);
 iter_num = 5;
 
 % LETTER
-% learning_rate = 1e5;
-% t0 = 1e5;
-% skip = 1e2;
-
-% MNIST 
-learning_rate = 1e3;
+learning_rate = 1e5;
+learning_rate_anchor = 1e3;
 t0 = 1e5;
 skip = 1e2;
+
+% MNIST 
+% learning_rate = 1e3;
+% t0 = 1e5;
+% skip = 1e2;
  
 count = skip;
 
@@ -31,7 +32,7 @@ factors_num = 10;
 % Lipschitz to noise ratio
 % control the number of neighbours
 
-LC = 0.2;
+LC = 0.5;
 rmse_dadk_llfm_test = zeros(iter_num, epoch); 
 rmse_dadk_llfm_train = zeros(iter_num, epoch); 
 
@@ -166,7 +167,7 @@ for i=1:iter_num
                 s = 2 * LC * (repmat(X, nearest_neighbor, 1) - anchors(anchor_idx, :));
                 base = -s * sum(weight.*y_anchor(u,:));
                 base = base + repmat(y_anchor(u,:)',1,p).* s*sum(weight);
-                anchors(anchor_idx,:) = anchors(anchor_idx,:) - learning_rate  / (idx + t0) * ((err(u)-1)*y(u)* base/(sum(weight).^2));
+                anchors(anchor_idx,:) = anchors(anchor_idx,:) - learning_rate_anchor  / (idx + t0) * ((err(u)-1)*y(u)* base/(sum(weight).^2));
 
             end
 
